8ff7c228950641877d2e0841ad586023784205f3
samuel-bunker <sbunker@email.sc.edu>
2022-02-27 22:46:11 -0500

    Updating last commit to close issues: closes #35 , closes #36 , closes #38

--- Source/Ophiuchus/FlipBook_Alien.cpp
+++ Source/Ophiuchus/FlipBook_Alien.cpp
@@ -1,4 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
+//Samuel Bunker: Creates class for our flipbook version of the alien enemy. This character utilizes the AI for chasing the player.
 
 
 #include "FlipBook_Alien.h"
